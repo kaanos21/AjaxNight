037ec68db9d37423f22eb39d1e820fdf76e7dbb0
kaanos21 <kaanonkaya@gmail.com>
2024-07-30 21:15:15 +0300

    Add project files.

---
+++ AjaxNight/Controllers/DefaultController.cs
@@ -0,0 +1,59 @@
+﻿using AjaxNight.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+
+namespace AjaxNight.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly Context _context;
+
+        public DefaultController(Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ProductList()
+        {
+            var values=_context.Products.ToList();
+            var jsonvalue=JsonConvert.SerializeObject(values);
+            return Json(jsonvalue);
+        }
+        public IActionResult CreateProduct(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            var values = JsonConvert.SerializeObject(product);
+            return Json(values);
+        }
+        public IActionResult DeleteProduct(int id)
+        {
+            var value=_context.Products.Find(id);
+            _context.Products.Remove(value);
+            _context.SaveChanges();
+            return NoContent();
+        }
+        [HttpGet]
+        public IActionResult GetProduct(int id)
+        {
+            var value = _context.Products.Find(id);
+            var jsonValues=JsonConvert.SerializeObject(value);
+            return Json(jsonValues);
+        }
+        [HttpPost]
+        public IActionResult UpdateProduct(Product product)
+        {
+            var value=_context.Products.Find(product.ProductId);
+            value.ProductName=product.ProductName;
+            value.Stock=product.Stock;
+            value.Price=product.Price;
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
---
+++ AjaxNight/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AjaxNight.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-COQ9ECD\\SQLEXPRESS;database=AjaxDbNight;integrated security=true");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
